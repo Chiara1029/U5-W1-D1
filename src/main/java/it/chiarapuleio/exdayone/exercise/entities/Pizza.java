7e8886fc107d71e40f523e03099bb5adb968f5e4
Chiara Puleio <chiarapuleio@yahoo.it>
2024-02-05 16:56:24 +0100

    completed, last commit

--- src/main/java/it/chiarapuleio/exdayone/exercise/entities/Pizza.java
+++ src/main/java/it/chiarapuleio/exdayone/exercise/entities/Pizza.java
@@ -14,21 +14,32 @@ public class Pizza  extends SuperMenu {
 
     public Pizza(String name, int kcal, double price) {
         super(name, kcal, price);
+        this.description = "Tomato and cheese";
     }
 
     public Pizza(String name, int kcal, double price, List<Toppings> toppings) {
         super(name, kcal, price);
         this.toppings = toppings;
-        this.description = "Tomato, Cheese, ";
+        this.description = "Tomato, Cheese";
     }
 
     @Override
     public String toString() {
+        StringBuilder toppingsStr = new StringBuilder();
+        if(toppings != null && !toppings.isEmpty()){
+            toppingsStr.append(", ");
+            for(Toppings topping : toppings){
+                if(topping != null){
+                    toppingsStr.append(topping.getName()).append(", ");
+                }
+            }
+            toppingsStr.setLength(toppingsStr.length() -2);
+        }
         return "Pizza{" +
-                "description=" + description + toppings +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
+                ", description=" + description + toppingsStr +
                 ", kcal=" + kcal +
                 ", price=" + price +
-                '}' + '\'' ;
+                '}' + '\n' ;
     }
 }
